8dfb569b91a0b7f1757ac44b42d7d71f5d798d2f
Mohamedjr711 <momosalahko7@gmail.com>
2023-03-16 19:09:59 -0700

    why description every time

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - is used for printf it an obeevous description
+ *
+ * Return : is 0 when sucessfull
+ */
+int main(void)
+{ /* main :is for return value */
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
