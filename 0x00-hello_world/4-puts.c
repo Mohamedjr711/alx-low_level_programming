c5c769c341727e3b49c39dcb51b08a74b085a526
Mohamedjr711 <momosalahko7@gmail.com>
2023-03-16 19:43:38 -0700

    finally

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - print line using put 
- *
- * Return : 0
- */
-
+*main-print line using puts
+*
+*Return : 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0); }
+return (0);
+}
