499097daa3101bbd5b751f55e51f07a1e9beea93
Mohamedjr711 <momosalahko7@gmail.com>
2023-03-18 12:29:46 -0700

    new one

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  *parameter: describe the parameter
  *Return: Always 1 (Success)
  */
-void  main(void)
+int  main(void)
 {
 printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
