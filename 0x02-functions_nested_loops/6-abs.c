8fc359ffb367f30b59abb9ed0652882b5dca66f0
Mohamedjr711 <momosalahko7@gmail.com>
2023-03-21 19:39:49 -0700

    seventh

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_islower - A program that prints a line with puts function
+ *lowercase character . another cases , shows 
+ * 0
+ *parameter: describe the parameter
+ *Return: 1 for lowercases character. 0 for the rest
+ */
+int _abs(int)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return ( i * -1);
+	}
+}
