1be4813f326fec1b696e17da9361991627b2868e
Mohamedjr711 <momosalahko7@gmail.com>
2023-03-21 18:43:20 -0700

    second

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - A program that prints a line with puts function
+ *Description: 'the program's description'
+ *parameter: describe the parameter
+ *Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+int i;
+for (i=97; i<=122; i++)
+{
+	_putchar(i);
+}
+_putchar('\n');
+}
