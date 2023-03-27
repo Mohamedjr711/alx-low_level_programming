84543b501fba4c05dc5e7f512efb15d1d895999d
Mohamedjr711 <momosalahko7@gmail.com>
2023-03-27 08:00:01 -0700

    easy mezzy

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - A program that prints a line with puts function
+ *Description: 'the program's description'
+ *parameter: describe the parameter
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar("\n");
+return (0);
+}
