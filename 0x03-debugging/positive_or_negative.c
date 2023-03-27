681aa1d19cc4057d75f67127a1d06d166555017a
Mohamedjr711 <momosalahko7@gmail.com>
2023-03-27 15:12:46 -0700

    shaan

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ *positive_or_negative - A program that prints a line with puts function
+ *Description: 'the program's description'
+ *@i: describe the parameter
+ *Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+if (i < 0)
+printf("%d is negative\n", i);
+else
+printf("%d is positive\n", i);
+return (0);
+}
