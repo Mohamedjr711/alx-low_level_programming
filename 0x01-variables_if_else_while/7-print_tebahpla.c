5476f1018947f4c99f2767f0b2d7319e8cdf1c42
Mohamedjr711 <momosalahko7@gmail.com>
2023-03-27 09:12:42 -0700

    lezzy

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - A program that prints a line with puts function
+ *Description: 'the program's description'
+ *parameter: describe the parameter
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+for (i = 'z'; i >= 'a'; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
