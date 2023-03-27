f17b4303f838832f1a08d26a23fe80b6407e06f7
Mohamedjr711 <momosalahko7@gmail.com>
2023-03-27 06:25:49 -0700

    first

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+#include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ *main - A program that prints a line with puts function
+ *Description: 'the program's description'
+ *parameter: describe the parameter
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
