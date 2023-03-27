8a335ed7dba530adc6b1480d55b92ffad6f65db1
Mohamedjr711 <momosalahko7@gmail.com>
2023-03-27 09:58:08 -0700

    negan

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - A program that prints a line with puts function
+ *Description: 'the program's description'
+ *parameter: describe the parameter
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+if (i == 0)
+putchar(i + '0');
+else
+putchar(i + '0');
+putchar(',');
+putchar(' ');
+}
