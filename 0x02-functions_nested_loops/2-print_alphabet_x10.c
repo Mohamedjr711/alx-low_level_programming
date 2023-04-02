50db5351f8e25687630ad33eac55209350fa6dfd
Mohamedjr711 <momosalahko7@gmail.com>
2023-04-02 05:35:24 -0700

    sorry world

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,12 @@ void print_alphabet_x10(void)
 {
 	int i;
 	char j;
-	for (j = 1; i <= 10 ; i++)
-	{
+for (j = 1; i <= 10; i++)
+{
 		for (j = 97; j <= 122; j++)
 		{
 			_putchar(j);
 		}
 		_putchar('\n');
-	}
-	
+}
 }
